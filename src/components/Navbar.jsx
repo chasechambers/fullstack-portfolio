c8ba2307e16eddbae0dd05926033b396aba7ce79
Chase <chambers.chase@gmail.com>
2023-08-21 08:51:05 -0400

    Another resume update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -124,7 +124,7 @@ export const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='https://drive.google.com/file/d/1P_D3kvK6Co3jcCkMNfXpSxP-e5SeOVzP/view?usp=sharing'
+              href='https://drive.google.com/file/d/1_wFXLBwuc9SJTiDYg9XO-eprHiP-b8mC/view?usp=sharing'
               target='_blank'
               rel='noreferrer'>
               Resume <BsFillPersonLinesFill size={30} />
